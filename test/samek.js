2cc70d06b24a30cfb2669ab44e85fa213ff1d134
David Durman <org@client.io>
2013-01-19 14:04:30 +0100

    run tests with Mocha + expect.js in both NodeJS and browsers

--- test/samek.js
+++ test/samek.js
@@ -1,62 +1,59 @@
-/**
- * Run: node switch.js  (in node)
- *      run.html?unit=samek (in browser)
- */
-
-(function(exports, undefined){
-    'use strict'
-
-    try {
-        core = require('./core');
-        Statechart = require('../lib/statechart');
-    } catch (e) {}
+try {
+    
+    _ = require('underscore');
+    Statechart = require('../lib/statechart');
+    expect = require('expect.js');
+    
+} catch (e) {}
 
 
-    var machine = core.Mixin({
-        // slots
-        myFoo: false,
-        // machine
-        initialState: "S0",
-        states: {
-            S0: {
-                init: "S1",
-                "E": { target: "S211" },
-                states: {
-                    S1: {
-                        init: "S11",
-                        "A": { target: "S1"   },
-                        "B": { target: "S11"  },
-                        "C": { target: "S2"   },
-                        "D": { target: "S0"   },
-                        "F": { target: "S211" },
-                        states: {
-                            S11: {
-                                "G": { target: "S211" },
-                                "H": {
-                                    guard: function(){ return this.myFoo; },
-                                    action: function(){ this.myFoo = false; }
-                                }
+var machine = _.extend({
+    
+    // slots
+    myFoo: false,
+    
+    // machine
+    initialState: "S0",
+    
+    states: {
+        S0: {
+            init: "S1",
+            "E": { target: "S211" },
+            states: {
+                S1: {
+                    init: "S11",
+                    "A": { target: "S1"   },
+                    "B": { target: "S11"  },
+                    "C": { target: "S2"   },
+                    "D": { target: "S0"   },
+                    "F": { target: "S211" },
+                    states: {
+                        S11: {
+                            "G": { target: "S211" },
+                            "H": {
+                                guard: function() { return this.myFoo; },
+                                action: function() { this.myFoo = false; }
                             }
                         }
-                    },
-                    S2: {
-                        init: "S21",
-                        "C": { target: "S1"  },
-                        "F": { target: "S11" },
-                        states: {
-                            S21: {
-                                init: "S211",
-                                "B": { target: "S211" },
-                                "H": {
-                                    guard: function(){ return !this.myFoo; },
-                                    action: function(){ this.myFoo = true; },
-                                    target: "S21"
-                                },
-                                states: {
-                                    S211: {
-                                        "D": { target: "S21" },
-                                        "G": { target: "S0"  }
-                                    }
+                    }
+                },
+                S2: {
+                    init: "S21",
+                    "C": { target: "S1"  },
+                    "F": { target: "S11" },
+                    states: {
+                        S21: {
+                            init: "S211",
+                            "B": { target: "S211" },
+                            "H": {
+                                guard: function() { return !this.myFoo; },
+                                action: function() { this.myFoo = true; },
+                                target: "S21"
+                            },
+                            states: {
+                                S211: {
+                                    "D": { target: "S21" },
+                                    "G": { target: "S0"  }
                                 }
                             }
                         }
@@ -64,147 +61,162 @@
                 }
             }
         }
-    }, Statechart.Statechart);
+    }
+    
+}, Statechart);
 
 
-    // Test
-    machine.run();
-    machine.dispatch("G");
-    machine.dispatch("G");
-    machine.dispatch("E");
-    machine.dispatch("G");
-    machine.dispatch("E");
-    machine.dispatch("G");
-    machine.dispatch("E");
-    machine.dispatch("G");
-    machine.dispatch("E");
-    machine.dispatch("G");
-    machine.dispatch("E");
-    machine.dispatch("G");
-    machine.dispatch("E");
-    machine.dispatch("G");
-    machine.dispatch("E");
+// Run the machine in debug mode and collect all messages describing state transitions.
+var samekStatesOrder = [];
 
-})(typeof exports !== "undefined" ? exports : test = {});
+machine.run({ debug: function(msg) {
 
-/**
-   Expected order of states:
+    samekStatesOrder.push(msg);
+    
+} });
 
-   [S0] enter
-   [S0] init
-   [S1] enter
-   [S1] init
-   [S11] enter
-   [S11] init
-   [S11] exit
-   [S1] exit
-   [S21] enter
-   [S211] enter
-   [S211] init
-   [S211] exit
-   [S21] exit
-   [S2] exit
-   [S0] exit
-   [S0] init
-   [S1] enter
-   [S1] init
-   [S11] enter
-   [S11] init
-   [S11] exit
-   [S1] exit
-   [S2] enter
-   [S21] enter
-   [S211] enter
-   [S211] init
-   [S211] exit
-   [S21] exit
-   [S2] exit
-   [S0] exit
-   [S0] init
-   [S1] enter
-   [S1] init
-   [S11] enter
-   [S11] init
-   [S11] exit
-   [S1] exit
-   [S2] enter
-   [S21] enter
-   [S211] enter
-   [S211] init
-   [S211] exit
-   [S21] exit
-   [S2] exit
-   [S0] exit
-   [S0] init
-   [S1] enter
-   [S1] init
-   [S11] enter
-   [S11] init
-   [S11] exit
-   [S1] exit
-   [S2] enter
-   [S21] enter
-   [S211] enter
-   [S211] init
-   [S211] exit
-   [S21] exit
-   [S2] exit
-   [S0] exit
-   [S0] init
-   [S1] enter
-   [S1] init
-   [S11] enter
-   [S11] init
-   [S11] exit
-   [S1] exit
-   [S2] enter
-   [S21] enter
-   [S211] enter
-   [S211] init
-   [S211] exit
-   [S21] exit
-   [S2] exit
-   [S0] exit
-   [S0] init
-   [S1] enter
-   [S1] init
-   [S11] enter
-   [S11] init
-   [S11] exit
-   [S1] exit
-   [S2] enter
-   [S21] enter
-   [S211] enter
-   [S211] init
-   [S211] exit
-   [S21] exit
-   [S2] exit
-   [S0] exit
-   [S0] init
-   [S1] enter
-   [S1] init
-   [S11] enter
-   [S11] init
-   [S11] exit
-   [S1] exit
-   [S2] enter
-   [S21] enter
-   [S211] enter
-   [S211] init
-   [S211] exit
-   [S21] exit
-   [S2] exit
-   [S0] exit
-   [S0] init
-   [S1] enter
-   [S1] init
-   [S11] enter
-   [S11] init
-   [S11] exit
-   [S1] exit
-   [S2] enter
-   [S21] enter
-   [S211] enter
-   [S211] init
+machine.dispatch("G");
+machine.dispatch("G");
+machine.dispatch("E");
+machine.dispatch("G");
+machine.dispatch("E");
+machine.dispatch("G");
+machine.dispatch("E");
+machine.dispatch("G");
+machine.dispatch("E");
+machine.dispatch("G");
+machine.dispatch("E");
+machine.dispatch("G");
+machine.dispatch("E");
+machine.dispatch("G");
+machine.dispatch("E");
 
-*/
+
+describe('Samek test machine', function() {
+
+    it('should transit states in the defined order', function() {
+
+        expect(samekStatesOrder).to.eql([
+
+            '[S0] enter',
+            '[S0] init',
+            '[S1] enter',
+            '[S1] init',
+            '[S11] enter',
+            '[S11] init',
+            '[S11] exit',
+            '[S1] exit',
+            '[S21] enter',
+            '[S211] enter',
+            '[S211] init',
+            '[S211] exit',
+            '[S21] exit',
+            '[S2] exit',
+            '[S0] exit',
+            '[S0] init',
+            '[S1] enter',
+            '[S1] init',
+            '[S11] enter',
+            '[S11] init',
+            '[S11] exit',
+            '[S1] exit',
+            '[S2] enter',
+            '[S21] enter',
+            '[S211] enter',
+            '[S211] init',
+            '[S211] exit',
+            '[S21] exit',
+            '[S2] exit',
+            '[S0] exit',
+            '[S0] init',
+            '[S1] enter',
+            '[S1] init',
+            '[S11] enter',
+            '[S11] init',
+            '[S11] exit',
+            '[S1] exit',
+            '[S2] enter',
+            '[S21] enter',
+            '[S211] enter',
+            '[S211] init',
+            '[S211] exit',
+            '[S21] exit',
+            '[S2] exit',
+            '[S0] exit',
+            '[S0] init',
+            '[S1] enter',
+            '[S1] init',
+            '[S11] enter',
+            '[S11] init',
+            '[S11] exit',
+            '[S1] exit',
+            '[S2] enter',
+            '[S21] enter',
+            '[S211] enter',
+            '[S211] init',
+            '[S211] exit',
+            '[S21] exit',
+            '[S2] exit',
+            '[S0] exit',
+            '[S0] init',
+            '[S1] enter',
+            '[S1] init',
+            '[S11] enter',
+            '[S11] init',
+            '[S11] exit',
+            '[S1] exit',
+            '[S2] enter',
+            '[S21] enter',
+            '[S211] enter',
+            '[S211] init',
+            '[S211] exit',
+            '[S21] exit',
+            '[S2] exit',
+            '[S0] exit',
+            '[S0] init',
+            '[S1] enter',
+            '[S1] init',
+            '[S11] enter',
+            '[S11] init',
+            '[S11] exit',
+            '[S1] exit',
+            '[S2] enter',
+            '[S21] enter',
+            '[S211] enter',
+            '[S211] init',
+            '[S211] exit',
+            '[S21] exit',
+            '[S2] exit',
+            '[S0] exit',
+            '[S0] init',
+            '[S1] enter',
+            '[S1] init',
+            '[S11] enter',
+            '[S11] init',
+            '[S11] exit',
+            '[S1] exit',
+            '[S2] enter',
+            '[S21] enter',
+            '[S211] enter',
+            '[S211] init',
+            '[S211] exit',
+            '[S21] exit',
+            '[S2] exit',
+            '[S0] exit',
+            '[S0] init',
+            '[S1] enter',
+            '[S1] init',
+            '[S11] enter',
+            '[S11] init',
+            '[S11] exit',
+            '[S1] exit',
+            '[S2] enter',
+            '[S21] enter',
+            '[S211] enter',
+            '[S211] init'
+            
+        ]);
+        
+    });
+    
+});
--- test/switch.js
+++ test/switch.js
@@ -1,38 +1,65 @@
-/**
- * Run: node switch.js  (in node)
- *      run.html?unit=switch (in browser)
- */
+try {
+    
+    _ = require('underscore');
+    Statechart = require('../lib/statechart');
+    expect = require('expect.js');
+    
+} catch (e) {}
 
-(function(exports, undefined){
-    'use strict'
 
-    try {
-         core = require('./core');
-         Statechart = require('../lib/statechart');
-    } catch (e) {}
-
-    var LightSwitch = core.Mixin({
-        initialState: "Out",
-        states: {
-            "Out": {
-                "on":  { target: "On"   },
-                "out": { target: "Out"  }
-            },
-            "On": {
-                "on":  { target: "On"  },
-                "out": { target: "Out" }
-            }
+var LightSwitch = _.extend({
+    
+    initialState: "Out",
+    states: {
+        "Out": {
+            "on":  { target: "On"   },
+            "out": { target: "Out"  }
+        },
+        "On": {
+            "on":  { target: "On"  },
+            "out": { target: "Out" }
         }
-    }, Statechart.Statechart);
+    }
+    
+}, Statechart);
 
 
-    LightSwitch.run();
+// Run the machine in debug mode and collect all messages describing state transitions.
+var switchStatesOrder = [];
 
-    LightSwitch.dispatch("out");
-    LightSwitch.dispatch("on");
-    LightSwitch.dispatch("on");
-    LightSwitch.dispatch("out");
+LightSwitch.run({ debug: function(msg) {
 
+    switchStatesOrder.push(msg);
+    
+} });
 
-})(typeof exports !== "undefined" ? exports : test = {});
+LightSwitch.dispatch("out");
+LightSwitch.dispatch("on");
+LightSwitch.dispatch("on");
+LightSwitch.dispatch("out");
 
+describe('LightSwitch test machine', function() {
+
+    it('should transit states in the defined order', function() {
+
+        expect(switchStatesOrder).to.eql([
+
+            '[Out] enter',
+            '[Out] init',
+            '[Out] exit',
+            '[Out] enter',
+            '[Out] init',
+            '[Out] exit',
+            '[On] enter',
+            '[On] init',
+            '[On] exit',
+            '[On] enter',
+            '[On] init',
+            '[On] exit',
+            '[Out] enter',
+            '[Out] init'
+            
+        ]);
+    });
+    
+});
